3cb93bff8a73ceeafe31fc3ca2c48894787b12f7
Fred Silva <fredericochopin@gmail.com>
2017-08-14 21:59:32 -0300

    Modifications for Heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 //Express app
 var app = express();
 
@@ -24,6 +26,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
     res.render('maintenance.hbs');
+    next();
 });
 
 /*
@@ -65,6 +68,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000...');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}...`);
 });
